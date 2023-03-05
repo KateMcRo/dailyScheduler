f6d4c372e82738f775e759b274633dabf6309d08
KateMcRo <katemcr0@gmail.com>
2023-03-05 14:25:56 -0800

    cleaned up, added comments, and asset

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,25 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-
+// prevents jquery from running until all elements on dom are ready
+$(document).ready(function () {
   // current hour in 24-hour time
   const currentHour = dayjs().hour()
   // using jquery to loop over each time-block element
    $(".time-block").each(function (){
     // storing each block in a variable
     const hourEl = $(this)
+    // variable for save buttons for each block
+    const saveBtnEl = hourEl.children(".saveBtn")
+    // variable for the text area for each block
+    const textAreaEl = hourEl.children(".description")
+    // variable for local storage key
+    const textId = hourEl.attr("id")
+    // sets value of text area to whatever is pulled from local storage
+    textAreaEl.val(localStorage.getItem(textId))
+    // function to save what the user inputs into the text area to local storage
+    saveBtnEl.on("click", function() {
+      const textValue = textAreaEl.val()
+      localStorage.setItem(textId, textValue)
+    })
+
     // storing the number portion of id in a string and converting to an interger
     const hourNum = parseInt($(this).attr("id").substr(5, 2), 10)
    
@@ -42,14 +42,10 @@ $(function () {
       hourEl.addClass("future")
     }
    })
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   // Uses dayjs to get the current date
   const currentDate = dayjs().format('MM/DD/YYYY')
   // Stores the p element from html
   const dateEl = $("#currentDay")
   // Sets the text of the p element to the current date
   dateEl.text(currentDate)
-});
+})
